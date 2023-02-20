cf570239f50f0576c018bb3dfb2997054f985f5d
lunaxm <125177814+lunaxm@users.noreply.github.com>
2023-02-20 22:59:09 +0800

    Tailwind CSS Boilerplate and Configuration
    https://tailwindcss.com/docs/installation

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "app/ecommerce/main.js",
+    "app/ecommerce/app.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
