4266fd931c2871ebf450862ed4eb68bc59e1008b
lunaxm <125177814+lunaxm@users.noreply.github.com>
2023-03-03 16:22:51 +0800

    Node server initialisation
    Initialise node server on port 3000
    Succesful case should log "Server is listening on port 3000" to terminal
    Requires installation on node npm

--- app/ecommerce/app.jsx
+++ app/ecommerce/app.jsx
@@ -2,3 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './app.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+<h1 id='title'>EatUp Ecommerce</h1>
--- app/ecommerce/main.js
+++ app/ecommerce/main.js
@@ -1,5 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx'
+//import React from "react";
+//import ReactDOM from "react-dom";
+//import App from "./App.jsx";
+const http = require("http");
+const port = 3000;
 
-ReactDOM.render (<App />, document.getElementById('App'));
+const server = http.createServer(function (req, res) {});
+
+server.listen(port, function (error) {
+  if (error) {
+    console.log(`Something went wrong: ${error}`);
+  } else {
+    console.log(`Server is listening on port ${port}`);
+  }
+});
+
+//ReactDOM.render(<App />, document.getElementById("App"));
