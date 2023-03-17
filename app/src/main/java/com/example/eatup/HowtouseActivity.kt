c73e79757f70e6b69372079f29476621c2116042
potatoeees <105065633+potatoeees@users.noreply.github.com>
2023-03-17 17:57:24 +0800

    how to use, contact us

---
+++ app/src/main/java/com/example/eatup/HowtouseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eatup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HowtouseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_howtouse)
+    }
+}
