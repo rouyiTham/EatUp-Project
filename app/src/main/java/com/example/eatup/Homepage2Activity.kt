528fb240d2dfc842165fd1f57410232db87bb6db
jmwongg <jiamienwong@gmail.com>
2023-03-24 14:02:10 +0800

    Linking Button

--- app/src/main/java/com/example/eatup/Homepage2Activity.kt
+++ app/src/main/java/com/example/eatup/Homepage2Activity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class Homepage2Activity : AppCompatActivity() {
     private lateinit var joinowBtn2 : Button
@@ -16,6 +17,30 @@ class Homepage2Activity : AppCompatActivity() {
         joinowBtn2 = findViewById(R.id.joinowBtn2)
         joinowBtn2.setOnClickListener{
             val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        val homebtn: TextView = findViewById(R.id.homebtn)
+        homebtn.setOnClickListener {
+            val intent = Intent(this,Homepage1Activity::class.java)
+            startActivity(intent)
+        }
+
+        val howtousebtn: TextView =findViewById(R.id.howtousebtn)
+        howtousebtn.setOnClickListener {
+            val intent= Intent(this,HowtouseActivity::class.java)
+            startActivity(intent)
+        }
+
+        val contactusbtn: TextView = findViewById(R.id.contactusbtn)
+        contactusbtn.setOnClickListener {
+            val intent = Intent(this,ContactusActivity::class.java)
+            startActivity(intent)
+        }
+
+        val joinnowwhite: Button = findViewById(R.id.joinnowwhite)
+        joinnowwhite.setOnClickListener {
+            val intent= Intent(this,Login::class.java)
             startActivity(intent)
         }
     }
